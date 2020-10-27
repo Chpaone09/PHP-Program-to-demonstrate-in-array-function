ab9e845bb9679c0950415bcd8919dd7bf326e9dc
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-27 22:06:18 +0530

    Program to demonstrate in_array function in PHP

---
+++ in_array.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+***********************************************************
+
+Topic   : Program to demonstrate in_array function in PHP.
+Author  : Chpaone09®
+Date    : Oct 27, 2020
+
+************************************************************
+*/
+
+$a = array(5,3,4,1,2);
+
+if(in_array(3,$a)){
+echo "found";
+}else{
+echo "not found";
+}
+
+?>
